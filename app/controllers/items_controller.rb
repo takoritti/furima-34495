3f545a8cc110aa2b2c1b1721868d18381b252071
takoritti <s94.owari.takashi@gmail.com>
2021-03-09 00:30:39 +0900

    削除機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -35,11 +35,11 @@ class ItemsController < ApplicationController
     end
   end
   
-  # def destroy
-  #   item = Item.find(params[:id])
-  #   item.destroy
-  #   redirect_to root_path
-  # end
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy
+    redirect_to root_path
+  end
 end
 
 private
